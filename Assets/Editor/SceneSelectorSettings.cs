3dd8fb14281999af5781cb619e6039dd6b0d402a
Jonathan ROGER <j.roger@boostmymail.com>
2022-10-03 10:03:13 +0200

    build(Utils): retrait du package unity pour embarquer directement les scripts dans le projet

---
+++ Assets/Editor/SceneSelectorSettings.cs
@@ -0,0 +1,19 @@
+﻿#if (UNITY_EDITOR)
+using UnityEditor;
+#endif
+
+namespace RogerFamily.Editor
+{
+    #if (UNITY_EDITOR)
+    [FilePath("Assets/SceneSelectorTool/Editor/SceneSelectorSettings.asset", FilePathAttribute.Location.ProjectFolder)]
+    public class SceneSelectorSettings : ScriptableSingleton<SceneSelectorSettings>
+    {
+        public string PreviousScenePath;
+
+        void OnDestroy()
+        {
+            Save(true);
+        }
+    }
+    #endif
+}
